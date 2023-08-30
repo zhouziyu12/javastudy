42e8ef4638950b97f57c5ff33f7cff8e8c420ade
zhouziyu12 <scyzz10@nottingham.edu.cn>
2023-08-30 21:57:51 +0800

    2023.8.30“ git commit -m 2023.8.30“

---
+++ javastudy/day02-basic/DAY02-basic/src/com/hema/type/typeconversionDemo1.java
@@ -0,0 +1,18 @@
+package com.hema.type;
+
+public class typeconversionDemo1 {
+    public static void main(String[] args){
+        byte a=12;
+        int b=a;
+        System.out.println(a);
+
+        System.out.println(b);
+        int c=100;
+        double d=c;
+        System.out.println(d);
+
+        char ch='a';
+        int i =ch;
+        System.out.println(i);
+    }
+}
--- javastudy/day02-basic/DAY02-basic/src/com/hema/variable/SACIIdemo1.java
+++ javastudy/day02-basic/DAY02-basic/src/com/hema/variable/SACIIdemo1.java
@@ -4,6 +4,8 @@ public class SACIIdemo1 {
     public static void main(String[] args) {
         //ASCII编码特点
         System.out.println('a'+10);
+      int a2=0141;//0开头当8进制
 
+    System.out.println(a2);
     }
 }

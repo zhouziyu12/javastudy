2d63930980361678908bb175aab668816ea06aac
zhouziyu12 <scyzz10@nottingham.edu.cn>
2023-08-30 22:11:14 +0800

    2023.8.30“ git commit -m 2023.8.312“

---
+++ javastudy/day02-basic/DAY02-basic/src/com/hema/type/demo2.java
@@ -0,0 +1,13 @@
+package com.hema.type;
+
+public class demo2 {
+    public static void main(String[] args) {
+        byte a = 10;
+        int b = 20;
+        long c = 30;
+        long rs = a+b+c;
+        System.out.println(rs);
+
+
+    }
+}

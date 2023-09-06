229cd24086e23ffff61641fe6a544663ee79db0b
zhouziyu12 <scyzz10@nottingham.edu.cn>
2023-09-06 11:05:42 +0800

    9.5 second commit

--- javastudy/case-exercise/case-exercise/src/exercise/Test2.java
+++ javastudy/case-exercise/case-exercise/src/exercise/Test2.java
@@ -1,7 +1,35 @@
 package exercise;
 
+import java.util.Random;
+
 public class Test2 {
     public static void main(String[] args) {
 
+        System.out.println(createcode(8));
     }
+    public static String createcode(int n ){
+        Random r = new Random();
+        String code="";
+        for (int i = 1; i <=n ; i++) {
+            int type = r.nextInt(3);
+                    switch (type){
+                        case 0:
+                            code+=r.nextInt(3);
+                            break;
+                        case 1 :
+                            char ch1=(char)(r.nextInt(26)+65);
+                            code+=ch1;
+                            break;
+                        case 2:
+                            char ch2=(char)(r.nextInt(25)+97);
+                            code+=ch2;
+                            break;
+                    }
+        }
+return code;
+
+
+    }
+
+
 }
